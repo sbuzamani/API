646ef62214f36abaa42324c26f3745e7adb6c376
Sibusiso Zamani <Sibusiso.Zamani@Euromonitor.com>
2019-01-21 15:30:07 +0200

    Add project files.

---
+++ MyLibrary.Domain.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLibrary.Domain.Integration.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
